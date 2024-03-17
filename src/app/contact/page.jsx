eabab769c0ff99bd3fe07b5aa1f784e3a964a9be
Atnan444 <116889151+Atnan-Tas@users.noreply.github.com>
2024-03-17 16:51:24 +0100

    page contact proto

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,9 +1,132 @@
-import React from 'react'
+"use client";
+import React, { useState } from "react";
+import InstaIcon from "../../../public/images/insta_icon.svg";
+import LinkedinIcon from "../../../public/linkedin-icon.svg";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import Link from "next/link";
+import Image from "next/image";
 
-const page = () => {
+const Contact = () => {
+  const [emailSubmitted, setEmailSubmitted] = useState(false);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const data = {
+      email: e.target.email.value,
+      subject: e.target.subject.value,
+      message: e.target.message.value,
+    };
+    const JSONdata = JSON.stringify(data);
+    const endpoint = "/api/send";
+
+    const options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSONdata,
+    };
+
+    const response = await fetch(endpoint, options);
+    const resData = await response.json();
+
+    if (response.status === 200) {
+      console.log("Message sent.");
+      setEmailSubmitted(true);
+    }
+  };
+
   return (
-    <div>page</div>
-  )
-}
+    <section
+      id="contact"
+      className=" md:grid-cols-2 my-12 md:my-12 py-24 gap-4 relative"
+    >
+        <Header />
+      <div className="bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-primary-900 to-transparent rounded-full h-80 w-80 z-0 blur-lg absolute top-3/4 -left-4 transform -translate-x-1/2 -translate-1/2"></div>
+      <div className="z-10">
+        <h5 className="text-xl font- ml-4 text-white my-2">
+          Contactez-moi !
+        </h5>
+        <p className="text-[#ADB7BE] ml-4 mb-4 max-w-md">
+          {" "}
+          Je suis actuellement étudiant en BUT Métier Multimédia Internet a Montbéliard, ma boîte de réception est toujours ouverte. 
+          Que vous ayez une question ou une proposition, je ferai de mon mieux pour vous répondre !
+        </p>
+        <div className="socials align-middle flex flex-row gap-2">
+          <Link className="ml-4  w-9" href="https://instagram.com/atnanzer/">
+            <Image src={InstaIcon} alt="Insta Icon" />
+          </Link>
+          <Link href="linkedin.com">
+            <Image src={LinkedinIcon} alt="Linkedin Icon" />
+          </Link>
+        </div>
+      </div>
+      <div className="ml-4 mr-4">
+        {emailSubmitted ? (
+          <p className="text-green-500 text-sm mt-2">
+            Votre mail a été envoyé !
+          </p>
+        ) : (
+          <form className="flex flex-col" onSubmit={handleSubmit}>
+            <div className="mb-6">
+              <label
+                htmlFor="email"
+                className="text-white block mb-2 text-sm font-medium"
+              >
+                Votre adresse mail
+              </label>
+              <input
+                name="email"
+                type="email"
+                id="email"
+                required
+                className="bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5"
+                placeholder="ex:bonjour@gmail.com"
+              />
+            </div>
+            <div className="mb-6">
+              <label
+                htmlFor="subject"
+                className="text-white block text-sm mb-2 font-medium"
+              >
+                Sujet
+              </label>
+              <input
+                name="subject"
+                type="text"
+                id="subject"
+                required
+                className="bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5"
+                placeholder="ex:Proposition de collaboration"
+              />
+            </div>
+            <div className="mb-6">
+              <label
+                htmlFor="message"
+                className="text-white block text-sm mb-2 font-medium"
+              >
+                Message
+              </label>
+              <textarea
+                name="message"
+                id="message"
+                className="bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5"
+                placeholder="ex:Je vous contact pour..."
+              />
+            </div>
+            <button
+              type="submit"
+              className="bg-purple-500  text-white font-medium py-2.5 px-5 rounded-lg w-80 mx-auto mb-4"
+            >
+              Envoyer
+            </button>
+          </form>
+        )}
+      </div>
+        <Footer />
+    </section>
+  );
+};
 
-export default page
+export default Contact;
