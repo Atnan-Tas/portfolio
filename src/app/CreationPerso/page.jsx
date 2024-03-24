4ee2a7946bbb42eff9fb81239d495f4e2fb37e06
Atnan444 <116889151+Atnan-Tas@users.noreply.github.com>
2024-03-24 16:46:47 +0100

    article perso et cassandre

--- src/app/CreationPerso/page.jsx
+++ src/app/CreationPerso/page.jsx
@@ -1,14 +1,56 @@
 import React from 'react'
+import Header from '../components/Header'
+import Footer from '../components/Footer'
+import Image from 'next/image'
 import Etoiles from '../components/etoiles'
 
 const Affiches = () => {
   return (
-     <section>
-       <Etoiles />  
-       <div className='relative z-30'>
-           <div>Création Perso</div>
-           </div>
-     </section>
+    <section>
+      <Etoiles />
+      <div className='relative z-30'>
+      <Header />
+      <h2 className="text-center text-4xl font-bold text-white mt-28 mb-8 md:mb-12">
+          Unified Champions Club
+      </h2>
+      <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
+        <Image className="rounded-xl" alt="à propos" src="/images/affiches.png" width={500} height={500} />
+        <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
+          <h2 className="text-4xl font-bold text-white mb-4">En quoi consistent mes affiches ?</h2>
+        <p className="text-justify lg:text-lg">
+        Pour mes affiches personnelles, j&apos;ai entrepris ce projet par passion, souhaitant rendre hommage 
+        à mes sportifs préférés qui ont façonné mon amour pour le sport, notamment le football et le basketball. 
+        Ces œuvres sont le reflet de mon admiration et de ma gratitude envers ces athlètes exceptionnels, 
+        ceux qui, par leur dévouement et leur talent, m&apos;ont inspiré et ont éveillé en moi une passion 
+        profonde pour ces disciplines. Chaque affiche est une célébration de leur héritage, un moyen pour moi 
+        d&apos;exprimer mon appréciation pour les moments inoubliables qu&apos;ils ont offerts à leurs fans et de 
+        reconnaître l&apos;impact significatif qu&apos;ils ont eu sur ma vie.
+        </p>
+        </div>
+      </div>
+      <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
+        <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
+        <h2 className="text-4xl font-bold text-white mb-4">Comment m&apos;y suis-je pris ?</h2>
+        <p className="text-justify lg:text-lg">
+        Animé par ma passion pour ces sportifs et mon désir d&apos;affiner mes compétences en design, 
+        je me suis lancé dans ce projet personnel. Mon objectif était double : rendre hommage à ces athlètes 
+        qui ont éveillé en moi un amour pour le sport et, dans le même temps, développer davantage mes 
+        aptitudes en design graphique. Pour y parvenir, j&apos;ai exploré diverses sources d&apos;inspiration 
+        et suivi de nombreux tutoriels, m&apos;efforçant d&apos;atteindre un niveau de réalisation qui me satisfasse pleinement.
+
+        L&apos;utilisation des outils et techniques abordés en cours a été fondamentale dans ce processus. 
+        Chaque étape du projet m&apos;a offert l&apos;opportunité de mettre en pratique et d&apos;approfondir 
+        les connaissances acquises en classe. Ce projet, bien que personnel, s&apos;est révélé être une extension 
+        précieuse de mon apprentissage, me permettant d&apos;explorer de nouvelles facettes du design et 
+        d&apos;appliquer de manière concrète les concepts étudiés.
+        </p>
+        </div>
+        <Image className="rounded-xl" alt="à propos" src="/images/lebron.png" width={350} height={350} />
+      </div>
+      </div>
+      <Footer /> 
+    </section>
+
   )
 }
 
--- src/app/cassandre/page.jsx
+++ src/app/cassandre/page.jsx
@@ -14,34 +14,34 @@ const Cassandre = () => {
           Carte Postale - Cassandre 
       </h2>
       <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
-        <Image className="rounded-xl" alt="à propos" src="/images/UC.png" width={500} height={500} />
+        <Image className="rounded-xl" alt="à propos" src="/images/affiche_WC.png" width={350} height={350} />
         <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
-          <h2 className="text-4xl font-bold text-white mb-4">Qu&apos;est-ce qu&apos;Unified Champions ?</h2>
+          <h2 className="text-4xl font-bold text-white mb-4">Pourquoi avoir fait ces affiches ?</h2>
         <p className="text-justify lg:text-lg">
-        L’ équipe E-Sport officielle de l’Université de Franche-Comté est un projet imaginé par Pascal Chatonnay, 
-        docteur en informatique, et Stéphane Laurent, professeur de droit, d’économie, de gestion et de marketing.
-        A la suite de plusieurs projets réalisés dans leur vie professionnelle hors université, en voyant beaucoup 
-        d’étudiants joueurs de jeux-vidéos et avec l’explosion mondiale du milieu de l’ E-Sport, ils ont alors lancé 
-        l’idée de la création d’une équipe de E-Sport composée des étudiants de l’Université de Franche-Comté, 
-        qui seraient intéressés pour faire de leur passe temps, quelque chose de plus grand qu’eux. 
-        Le but premier serait de participer à la Grosse Ligue League of Legends avec notre propre équipe universitaire 
-          et par la suite se développer à travers les compétitions sur différents jeux compétitifs.
+        Dans le cadre de notre formation en design, nous avons exploré l&apos;œuvre emblématique d&apos;Adolphe Mouron Cassandre, 
+        reconnu pour son style avant-gardiste et son impact significatif sur l&apos;affichage publicitaire et graphique du 20e siècle.
+         Inspirés par la maîtrise de Cassandre dans l&apos;utilisation de formes audacieuses, de compositions équilibrées et d&apos;une 
+         typographie innovante, nous avons été invités à créer nos propres affiches, en nous appropriant les principes de son 
+         art. Ce projet avait pour but de non seulement honorer l&apos;héritage de Cassandre mais aussi de repousser les limites 
+         de notre créativité en intégrant ses techniques à notre vision contemporaine. Cet exercice nous a permis de comprendre 
+         en profondeur l&apos;importance de l&apos;histoire du design dans la pratique actuelle et de voir comment les styles 
+         historiques peuvent être réinterprétés pour rester pertinents dans le monde moderne.
         </p>
         </div>
       </div>
       <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
         <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
-        <h2 className="text-4xl font-bold text-white mb-4">Quel est notre but durant ce projet ?</h2>
+        <h2 className="text-4xl font-bold text-white mb-4">L&apos;exposition Universelle - Riyad 2030</h2>
         <p className="text-justify lg:text-lg">
-        Le but de notre groupe durant ce projet, était de designer multiples éléments graphiques pour l’équipe, et ses
-        réseaux sociaux. Nous avons donc créé un logo, des bannières, des overlays pour les lives, des templates pour les
-        réseaux sociaux, des maquettes de site internet, des maquettes d’application mobile, et des goodies pour les
-        joueurs. Nous avons également fait toute la communication autour de l’équipe, mais le point le plus important,
-        était de créer un site internet pour l’équipe, qui permettrait de centraliser toutes les informations sur l’équipe,
-        mais également une boutique en ligne pour vendre les goodies que nous avons créé.
+        Pour mon affiche sur l&apos;Exposition Universelle de Riyad en 2030, j&apos;ai choisi de m&apos;inspirer du style de Cassandre, 
+        célèbre pour ses compositions publicitaires captivantes. J&apos;ai voulu mettre en avant les monuments emblématiques 
+        de Riyad, en leur donnant une allure tropicale qui contraste avec l&apos;aridité habituellement associée à la région. 
+        Cette approche vise à évoquer une vision futuriste et accueillante de la ville. En guise de clin d&apos;œil subtil à 
+        la culture locale, j&apos;ai inclus une signature très discrète du Al-Nassr Football Club, intégrant ainsi un élément 
+        contemporain qui reflète l&apos;esprit communautaire et la passion sportive de la ville.
         </p>
         </div>
-        <Image className="rounded-xl" alt="à propos" src="/images/Article_details.png" width={500} height={500} />
+        <Image className="rounded-xl" alt="à propos" src="/images/affiche_riyad.png" width={350} height={350} />
       </div>
       </div>
       <Footer /> 
